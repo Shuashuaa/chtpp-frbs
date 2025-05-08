93f68dd6af6bdaefac6e1edde4ca715a317b5bd8
Jovi Joshua Tania <jovijoshuatania027@gmail.com>
2025-05-08 11:17:37 +0800

    added a firebaseAnalytics to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { analytics  } from '@/firebase';
+
+const app = createApp(App);
+app.provide('firebaseAnalytics', analytics);
+
+app.mount('#app')
