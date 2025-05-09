4a571fe104c58b7219b4e7f510c6fc8c46952bed
Jovi Joshua Tania <jovijoshuatania027@gmail.com>
2025-05-09 08:53:08 +0800

    analytics feature removed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { analytics  } from '@/firebase';
+// import { analytics  } from '@/firebase';
 
 const app = createApp(App);
-app.provide('firebaseAnalytics', analytics);
+// app.provide('firebaseAnalytics', analytics);
 
 app.mount('#app')
